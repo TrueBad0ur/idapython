203da70b733c823151a97c11f7d47eb9a01ee640
Andrey <34392986+TrueBad0ur@users.noreply.github.com>
2023-04-21 15:59:21 +0300

    Create rename_dwords_to_addresses_2.py

---
+++ rename_dwords_to_addresses_2.py
@@ -0,0 +1,17 @@
+# rename dwords code
+# 004166E4 dword_4166E4 dd 419498h
+# 004166E8 dword_4166E8 dd 4194A8h
+
+# .data:00419498 somefunc1 db 'somefunc1',0
+# .data:004194A8 somefunc2 db 'somefunc2',0
+
+ea = get_screen_ea()
+
+for i in range(1):
+
+    var_to_rename = ea
+    string_addr = ida_bytes.get_dword(ea)
+    name = get_name(string_addr)
+    set_name(var_to_rename, "_" + name)
+    
+    ea += 4
